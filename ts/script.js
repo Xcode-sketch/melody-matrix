0fc37ec149e26e173c8c4faf90e664ca49fd1ed1
Eduardo Barbosa Silva <eduardobarbosasilva.oficial@outlook.com>
2025-09-13 02:29:13 -0300

    feat: modo claro adicionado + inicio da criação de cards

--- ts/script.js
+++ ts/script.js
@@ -1,89 +1,71 @@
-const whiteMode : Element | null = document.querySelector('.theme');
-const body = document.body
-const imageInput : HTMLInputElement | null = document.querySelector('#image-input');
-const createBtn : HTMLInputElement | null = document.querySelector('#createBtn');
-const tittleInput : HTMLInputElement | null = document.querySelector('#input-tittle');
-const durationInput : HTMLInputElement | null = document.querySelector('#input-duration');
+"use strict";
+const whiteMode = document.querySelector('.theme');
+const body = document.body;
+const imageInput = document.querySelector('#image-input');
+const createBtn = document.querySelector('#createBtn');
+const tittleInput = document.querySelector('#input-tittle');
+const durationInput = document.querySelector('#input-duration');
 const cardContainer = document.querySelector('.card-storage');
-
-
-
-function themeToggle() : void{
-    const savedTheme: string | null = localStorage.getItem('current-theme');
-    
-    if(savedTheme === 'true') {
+function themeToggle() {
+    const savedTheme = localStorage.getItem('current-theme');
+    if (savedTheme === 'true') {
         body.classList.add('white-mode');
-        
     }
 }
 whiteMode?.addEventListener('click', () => {
-    const isTheme : boolean | null = body.classList.toggle('white-mode');
+    const isTheme = body.classList.toggle('white-mode');
     localStorage.setItem('current-theme', String(isTheme));
-
     const themeIco = document.querySelector('#theme-ico');
-    if(isTheme == true) {
+    if (isTheme == true) {
         themeIco?.classList.remove('fa-sun');
         themeIco?.classList.add('fa-moon');
-    } else {
+    }
+    else {
         themeIco?.classList.remove('fa-moon');
         themeIco?.classList.add('fa-sun');
     }
-
-})
+});
 themeToggle();
-
 createBtn?.addEventListener('click', () => {
     const tittle = tittleInput?.value;
     const duration = durationInput?.value;
     const imageArchive = imageInput?.value;
-
-
     if (!tittle || !duration || !imageArchive) {
-    alert ("Por favor, preencha todos os campos antes de criar um card");
-    return;
+        alert("Por favor, preencha todos os campos antes de criar um card");
+        return;
     }
-
     const reader = new FileReader();
-
     reader.onload = (event) => {
-        const imageUrl = event.target?.result as string;
-
+        const imageUrl = event.target?.result;
         cardCreate(tittle, duration, imageUrl);
-
         clearForm();
-    }
-})
-
-function cardCreate(tittle : string, duration : string, imageUrl : string) {
+    };
+});
+function cardCreate(tittle, duration, imageUrl) {
     const cardDiv = document.createElement('div');
     cardDiv.classList.add('card');
-
-    const image: HTMLImageElement = document.createElement('img');
+    const image = document.createElement('img');
     image.src = imageUrl;
-
-    const cardImage: HTMLDivElement = document.createElement('div');
+    const cardImage = document.createElement('div');
     cardImage.className = 'card-image';
-
-    const cardText: HTMLDivElement = document.createElement('div');
+    const cardText = document.createElement('div');
     cardText.className = 'card-text';
-
     const duracao = document.createElement('p');
     duracao.textContent = duration;
-    
     const titulo = document.createElement('h3');
     titulo.textContent = tittle;
-
     cardImage.appendChild(image);
     cardText.appendChild(duracao);
     cardText.appendChild(titulo);
     cardDiv.appendChild(cardImage);
     cardDiv.appendChild(cardText);
-
     cardContainer?.appendChild(cardDiv);
 }
-
 function clearForm() {
-    if(tittleInput)tittleInput.value = '';
-    if(durationInput)durationInput.value = '';
-    if(imageInput)imageInput.value = '';
+    if (tittleInput)
+        tittleInput.value = '';
+    if (durationInput)
+        durationInput.value = '';
+    if (imageInput)
+        imageInput.value = '';
 }
